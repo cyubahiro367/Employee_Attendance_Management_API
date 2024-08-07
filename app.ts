de06635d9cbe8d59f3ce12a4c3f471d6bacfd1a5
cyubahiro367 <theotime.c@kudibooks.com>
2024-08-07 10:17:14 +0200

    connected to database

--- app.ts
+++ app.ts
@@ -1,14 +1,22 @@
 import express, {Express, Response} from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app: Express = express();
-const port: number = 3000;
+const port: number = parseInt(process.env.PORT || '3000', 10);
+const databaseUrl: string = process.env.DATABASE_URL || '';
+
+mongoose.connect(databaseUrl).then(() => {
+  console.log(`App running at http://localhost:${port}`);
+  app.listen(port);
+}).catch((error) => {
+  console.log(error);
+});
 
 app.use(express.json());
 
 app.get('/', (res: Response) => {
   res.send('Hello World! again');
-});
-
-app.listen(port, () => {
-  console.log(`App running at http://localhost:${port}`);
 });
--- dist/app.js
+++ dist/app.js
@@ -4,12 +4,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const mongoose_1 = __importDefault(require("mongoose"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const app = (0, express_1.default)();
-const port = 3000;
+const port = parseInt(process.env.PORT || '3000', 10);
+const databaseUrl = process.env.DATABASE_URL || '';
+mongoose_1.default.connect(databaseUrl).then(() => {
+    console.log(`App running at http://localhost:${port}`);
+    app.listen(port);
+}).catch((error) => {
+    console.log(error);
+});
 app.use(express_1.default.json());
 app.get('/', (res) => {
     res.send('Hello World! again');
-});
-app.listen(port, () => {
-    console.log(`App running at http://localhost:${port}`);
 });
